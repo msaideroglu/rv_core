991b4f95652dc4c04c1d54a9ce874f2bbc9a4fe3
msaideroglu <msaideroglu@gmail.com>
2024-10-16 00:14:53 +0300

    added sign bit feature

--- rtl/rv_alu.sv
+++ rtl/rv_alu.sv
@@ -6,18 +6,23 @@ module rv_alu(
 
   always_comb begin
     case (aluctrl_i) 
-      4'b0000:  result_o = a_i - b_i;                   // sub
-      4'b0001:  result_o = a_i + b_i;                   // add ,  addi
-      4'b0010:  result_o = a_i << b_i[4:0];             // sll ,  slli 
-      4'b0011:  result_o = (a_i < b_i) ? 32'b1 : 32'b0; // slt ,  slti 
-      4'b0100:  result_o = (a_i < b_i) ? 32'b1 : 32'b0; // sltu, sltiu
-      4'b0101:  result_o = a_i ^ b_i;                   // xor ,  xori
-      4'b0110:  result_o = a_i >>> b_i[4:0];            // sra ,  srai 
-      4'b0111:  result_o = a_i >> b_i[4:0];             // srl ,  srli 
-      4'b1000:  result_o = a_i | b_i;                   // or  ,   ori
-      4'b1001:  result_o = a_i & b_i;                   // and ,  andi
-      default:  result_o = 32'bx;                       // ???
+      4'b0000:  result_o = a_i - b_i;                                     // sub
+      4'b0001:  result_o = a_i + b_i;                                     // add ,  addi
+      4'b0010:  result_o = a_i << b_i[4:0];                               // sll ,  slli (unsigned) 
+      4'b0011:  result_o = (signed'(a_i) < signed'(b_i)) ? 32'b1 : 32'b0; // slt ,  slti (  signed)
+      4'b0100:  result_o = (a_i < b_i) ? 32'b1 : 32'b0;                   // sltu, sltiu (unsigned)
+      4'b0101:  result_o = a_i ^ b_i;                                     // xor ,  xori
+      4'b0110:  result_o = signed'(a_i) >>> b_i[4:0];                     // sra ,  srai (  signed)
+      4'b0111:  result_o = a_i >> b_i[4:0];                               // srl ,  srli (unsigned)
+      4'b1000:  result_o = a_i | b_i;                                     // or  ,   ori
+      4'b1001:  result_o = a_i & b_i;                                     // and ,  andi
+      default:  result_o = 32'bx;                                         // ???
     endcase
   end
 
 endmodule
+
+
+
+
+
